caca30c8646fea5813ece42176a548489c2e196b
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-08-12 17:22:36 -0400

    Completed first part of exercise

--- js/external.js
+++ js/external.js
@@ -1,3 +1,22 @@
 'use strict';
-console.log('Hello from External JS')
-alert('Hey this is definitely hard')
+console.log('Hello from External JS');
+alert('Welcome to my website!');
+var favColor = prompt('What is your favorite color?');
+console.log('User entered:'  +favColor);
+alert('I love the color ' + favColor + '!')
+var theLittleMermaid = 3 * 3;
+theLittleMermaid = 3 * 3;
+console.log('theLittleMermaid');
+prompt('How long was The Little Mermaid kept')
+alert('payout ' + theLittleMermaid)
+var brotherBear = 5 * 3;
+brotherBear = 5 * 3;
+prompt('How long was Brother Bear kept')
+alert('payout ' + brotherBear)
+console.log('brotherBear');
+var hercules = 1 * 3;
+prompt('How long was Hercules kept')
+console.log('hercules');
+alert('payout ' + hercules)
+var payout = 'theLittleMermaid' + 'brotherBear' + 'hercules';
+console.log('payout');
