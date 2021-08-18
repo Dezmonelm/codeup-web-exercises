8def2efd7d84cfa10fb1f898123573a3ae50f20c
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-08-18 01:32:14 -0400

    Super slpooy code

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,13 +1,46 @@
 'use strict'
+//
+// console.log('Hellos there!');
+//
+// function showMultiplicationTable (num) {
+//         console.log(showMultiplicationTable(num));
+//      for(var num = 1; num < 10; num++) {
+//          console.log(showMultiplicationTable());
+//          if(num == 1) Console.log(num);{
+//              else if {
+//              (num == 2)
+//              }
+//              else if {
+//              (num == 3)
+//                  }
+//              else if {
+//              (num == 4)
+//                  }
+//              else if {
+//              (num == 5)
+//                  } else; alert('done');
+//
+//
+//              }
+//          }
+//
+//
+//
+//      }
+// }
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to
+// the console whether each number is odd or even. For example:
 
-console.log('Hellos there!');
+for (var num = 1; num < 10; num++) {
+    console.log(num + 1);
+} while (num = Math.random());
+   alert(num === % 0 + 'Even')
 
-function showMultiplicationTable (num) {
-        console.log(showMultiplicationTable(num));
-     for(var num = 1; num < 10; num++) {
-         // if(num )
-        console.log(showMultiplicationTable());
+// Create a for loop that uses console.log to create the output shown below.
 
+for (var i =100; i < 100; i - 5);
+    while(i = 100 ) {
+        console.log('fives' + i)
+    }
 
-     }
-}
+    
---
+++ JS/while.js
@@ -0,0 +1,33 @@
+'use strict'
+
+console.log('hi')
+// var x = 2;
+// while(true) {
+//     alert('number double');
+//     x = x + x;
+//     if(x === 2) {
+//     if(x === 65536){
+//
+//     }
+//     }
+// }
+
+// Do While Loop
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random ' +
+// 'number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop ' +
+// 'your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her ' +
+// 'clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the ' +
+// 'random numbers for this exercise.
+
+var min = 50;
+var max 100;
+var number =  function randomNum(min, max) {
+    return Math.random() * (randommax - min + 1))
+var number;
+
+} do number(){
+    console.log(number + 'how many cones do you want?')
+    while {
+
+    }
+}
