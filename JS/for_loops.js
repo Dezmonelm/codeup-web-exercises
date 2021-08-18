882ef8853ac701e45afb234db30d0925a6f609ef
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-08-17 21:56:23 -0400

    CONNOT COMPUTE!

---
+++ JS/for_loops.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log('Hellos there!');
+
+function showMultiplicationTable (num) {
+        console.log(showMultiplicationTable(num));
+     for(var num = 1; num < 10; num++) {
+         // if(num )
+        console.log(showMultiplicationTable());
+
+
+     }
+}
