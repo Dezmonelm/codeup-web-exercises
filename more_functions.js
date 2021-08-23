15a537e3e2afc8bb8c741ea7cd6f4f6a1c7b5b46
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-08-22 22:11:59 -0400

    still workin

--- more_functions.js
+++ more_functions.js
@@ -54,16 +54,33 @@ console.log(addThree());
 function sayString(input) {
     return(input)
 }
-console.log(input);
+console.log(sayString());
 
 //     Challenge Function Drills
 //
 // Write a function called identity(input) that takes in an argument called input and returns that input.
-//
+
+function identity(input) {
+    return (input);
+
+}
+console.log(identity());
 //     Write a function called getRandomNumber(min, max) that returns a random number between min and max values sent to that function call.
-//
+
+function getRandomNUmber(min, max) {
+    var min = Number;
+    var max = NUmber;
+    return Math.Random() * (max - min) + min;
+}
+console.log();
+
+
 // Write a function called first(input) that returns the first character in the provided string.
 //
+function first(input) {
+    return(charAt());
+}
+console.log(chatAt());
 //     Write a function called last(input) that returns the last character of a string
 //
 // Write a function called rest(input) that returns everything but the first character of a string.
