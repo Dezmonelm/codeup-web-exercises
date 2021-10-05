39b59f47b7eace53407e124509b9cb5507050f5e
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-05 12:26:15 -0400

    My first blog post

--- js/mapbox-lecture-handout.js
+++ js/mapbox-lecture-handout.js
@@ -13,15 +13,29 @@
 
 //TODO: Set map to the San Antonio area using the coordinates [-98.4861, 29.4252]
 
-// mapboxgl.accessToken = YOUR_API_TOKEN_HERE;
-// console.log(mbKey);
+mapboxgl.accessToken = mapboxApiKey;
+console.log(mapboxApiKey);
 
-
+// var map = new mapboxgl.Map(
+//     {
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/streets-v11',
+//         center: [-98.4861, 29.4252],
+//         zoom: 10
+//     }
+// )
 
 
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the MapBox docs! (10 mins~)
 
 
+   var mapOptions = {
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-98.4861, 29.4252],
+        zoom: 10,
+    }
+var map = new mapboxgl.Map(mapOptions);
 
 
 /**********************************************
@@ -33,14 +47,27 @@
 
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-98.4861, 29.4260]. This marker will mark the Alamo on our map.
-
+var marker = new mapboxgl.Marker()
+    .setLngLat([-98.4916, 29.4260])
+    .addTo(map);
 
 // TODO TOGETHER: Change the color of the marker
 
-
+var marker = new mapboxgl.Marker({color: 'orange'})
+    .setLngLat([-98.4916, 29.4260])
+    .addTo(map);
 // TODO: Make a new marker! Let's experiment with the color and setting the LngLat [how about a marker for Codeup San Antone? -98.4895, 29.4267 Codeup Dallas? -96.8056, 32.7786]
 
-// TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
+var stateFarmArena = new mapboxgl.Marker(
+    {
+    color: 'lightBlue',
+        // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
+         draggable: true
+}
+)
+    .setLngLat([ -84.397331, 33.757290])
+    .addTo(map);
+
 
 /**********************************************
  * 					POPUPS
@@ -49,16 +76,36 @@
 // Popup docs --> https://docs.mapbox.com/mapbox-gl-js/api/#popup
 
 
+
 // TODO TOGETHER: Add a popup to the map over San Antonio's Codeup. Set the html as a paragraph that says "Codeup Rocks!"
+
+var popup = new mapboxgl.Popup()
+    .setLngLat([-98.4895, 29.4267])
+    .setHTML('<h6><em>Codeup ROCKS!</em></h6>')
+    .addTo(map);
 
 
 // TODO TOGETHER: We'll comment out the popup we just added. Next, let's add a popup to the Alamo marker!
+var alamoPopup = new mapboxgl.Popup()
+    .setHTML('<p>Remember the Alamo!</p>')
+    .addTo(map)
 
+marker.setPopup(alamoPopup)
+
+// OTHER STUFF
+
+alamoPopup.on('close', function () {
+    console.log('the popup was closed!');
+})
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup? Choose one and experiment with implementing that option to a popup!
 // TODO: Try setting the text of a popup by using ".setText()" instead of ".setHTML()" - what happens with HTML tags between the two?
 
+var alamoPopup = new mapboxgl.Popup()
+    .setText('<p>Remember the Alamo!</p>')
+    .addTo(map)
 
+marker.setPopup(alamoPopup)
 
 /**********************************************
  * 					Geocoder
@@ -73,11 +120,48 @@
 //  *      // do something with results
 //  *  })
 
+// geocode('8646 Sunny Oaks, San Antonio, TX', mapboxApiKey).then(function (results) {
+//
+//     console.log(results);
+// })
+//
+// geocode('600 Navarro St. #600, San Antonio, Texas 78205', mapboxApiKey).then(function (results) {
+//     console.log(results);
+//     map.setCenter(results);
+// })
 //TODO: Using the geocode method above, add a marker at Codeup to the map
 
+// geocode('600 Navarro St. #600, San Antonio, Texas 78205', mapboxApiKey).then(function (results) {
+//
+//     var popup = new mapboxgl.Popup()
+//         .setHTML('<em>Thanks</em>')
+//
+//     new mapboxgl.Marker()
+//         .setLngLat(results)
+//         .setPopup(popup)
+//         .addTo(map)
+// })
 
 //TODO: Instead of setCenter try using map.jumpTo()
-//TODO: Instead of setCenter try using map.flyTo()
+
+geocode('Portland, Maine', mapboxApiKey).then(function (results) {
+    var popup = new mapboxgl.Popup()
+        .setHTML('<em>Thanks</em>')
+
+    new mapboxgl.Marker()
+        .setLngLat(results)
+        .setPopup(popup)
+        .addTo(map)
+
+        // map.jumpTo({center: results})
+
+    //TODO: Instead of setCenter try using map.flyTo()
+        map.flyTo({center: results})
+
+
+});
+
+
 
 
 
@@ -88,6 +172,11 @@
 // *      // do something with results
 // *  })
 
+
+reverseGeocode({lat: 29.4260, Lng: -98.4861},
+    mapboxApiKey).then(function (results){
+        console.log(results);
+    })
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 
 
