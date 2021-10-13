34f5055321fd2c6b9fdbcf853eb643973ba7fb81
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-13 00:35:58 -0400

    konami

--- js/jquery.js
+++ js/jquery.js
@@ -43,45 +43,45 @@
 
 // var sound = new audio('/audio/')
 //use the variable tp target sound
-var sliceImg = null;
-var bombSound = null;
-$(document).ready(function () {
-    var userInput = prompt('You have to follow directions! Do you understand? (yes/no)');
-    userInput = userInput.toLowerCase();
-    var explode = $('button');
-    console.log(userInput);
-    if (userInput === 'yes') {
-        // add sound
-        alert('Let\'s begin!');
-        userInput = prompt('Enter the konami code: "↑ ↑ ↓ ↓ ← → ← →" b a enter');
-        console.log(userInput);
-        var konamiCode = 'ArrowUp ArrowUp ArrowDown ArrowDown Arrowleft ArrowRight Arrowleft ArrowRight b a Enter';
-        if (userInput === 'yes') {
-            userInput = '';
-            $('body').keyup(function (event) {
-                console.log(event.key);
-                if (userInput === konamiCode) {
-                    console.log('cool thing');
-                }
-                // event.preventDefault();
-                userInput = userInput + event.key;
-                console.log(userInput);
-            });
-
-        }
-        explode.click(function (e) {
-            e.preventDefault();
-            $('#command1').css('color', 'green');
-        });
-
-        console.log('make a sound');
-    } else {
-        // add action
-        console.log('explode');
-        explode.click(function () {
-            $('.fw-bold').css('color', 'red')
-        })
-    }
+// var sliceImg = null;
+// var bombSound = null;
+// $(document).ready(function () {
+//     var userInput = prompt('You have to follow directions! Do you understand? (yes/no)');
+//     userInput = userInput.toLowerCase();
+//     var explode = $('button');
+//     console.log(userInput);
+//     if (userInput === 'yes') {
+//         // add sound
+//         alert('Let\'s begin!');
+//         userInput = prompt('Enter the konami code: "↑ ↑ ↓ ↓ ← → ← →" b a enter');
+//         console.log(userInput);
+//         var konamiCode = 'ArrowUp ArrowUp ArrowDown ArrowDown Arrowleft ArrowRight Arrowleft ArrowRight b a Enter';
+//         if (userInput === 'yes') {
+//             userInput = '';
+//             $('body').keyup(function (event) {
+//                 console.log(event.key);
+//                 if (userInput === konamiCode) {
+//                     console.log('cool thing');
+//                 }
+//                 // event.preventDefault();
+//                 userInput = userInput + event.key;
+//                 console.log(userInput);
+//             });
+//
+//         }
+//         explode.click(function (e) {
+//             e.preventDefault();
+//             $('#command1').css('color', 'green');
+//         });
+//
+//         console.log('make a sound');
+//     } else {
+//         // add action
+//         console.log('explode');
+//         explode.click(function () {
+//             $('.fw-bold').css('color', 'red')
+//         })
+//     }
     // event.preventDefault;
 
     // var userInput = $('#input');
