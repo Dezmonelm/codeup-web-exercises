f1475bae48f7a4fffbe5448f6d212fac5d05f574
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-14 16:37:57 -0400

    exercise review

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,18 +39,21 @@ const users = [
 // Create a file named map-filter-reduce.js in your js directory and copy the users data below into it.
 //     Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 
-let threeOrMoreLangauges = users.filter(function (num) {
-    return num.languages.length >= 3;
+let threeOrMoreLanguages = users.filter(function (person) {
+    return person.languages.length >= 3;
 });
 
-console.log(threeOrMoreLangauges);
+// let threeOrMoreLangauges = users.filter((person) => user.languages.length > 2)
+//
+// console.log(threeOrMoreLangauges);
 
 //     Use .map to create an array of strings where each element is a user's email address
 
-let usersEmail = users.map(function (address) {
-    return address.email;
-});
+// let usersEmail = users.map(function (address) {
+//     return address.email;
+// });
 
+let usersEmail = users.map((address) => address.email);
 console.log(usersEmail);
 
 // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
@@ -78,9 +81,23 @@ console.log(longestEmail);
 
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
-let usersStingNames = users.reduce(function (stringNames, userNames) {
-    return
+// let usersStingNames = users.reduce(function (stringNames, userNames,index) {
+//     if(index != users.length - 1){
+//         return stringNames + userNames.name + (', ');
+//     }
+//     return stringNames + userNames.name + ('. ');
+// }, 'Instructors: ');
+// console.log(usersStingNames);
+
+let namestring = users.map(function (user) {
+    return user.name;
+}).join(', ');
+console.log(namestring);
+
+// Use .reduce to get the unique list of languages from the list of users.
+
+let uniqueLanguage = users.reduce(function (langList, userList) {
+    return langList.users.languages
 });
 
-console.log(usersStingNames);
-// Use .reduce to get the unique list of languages from the list of users.
+console.log(uniqueLanguage);
